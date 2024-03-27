15f94bfd904d4422338f51815165124152305aa1
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-03-27 20:35:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    if (e.target.value === "C") {
+      setValue("");
+      setResult("");
+    } else if (e.target.value === "=") {
+      setResult(eval(value));
+    } else {
+      setValue((prev) => prev + e.target.value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="heading">
+        <h1>React Calculator</h1>
+      </div>
+      <div className="input">
+        <input type="text" value={value} readOnly></input>
+      </div>
+      <div className="result">{result}</div>
+      <div className="container">
+        <div className="butt-grid">
+          <button value="7" onClick={handleClick}>
+            7
+          </button>
+          <button value="8" onClick={handleClick}>
+            8
+          </button>
+          <button value="9" onClick={handleClick}>
+            9
+          </button>
+          <button value="+" onClick={handleClick}>
+            +
+          </button>
+          <button value="4" onClick={handleClick}>
+            4
+          </button>
+          <button value="5" onClick={handleClick}>
+            5
+          </button>
+          <button value="6" onClick={handleClick}>
+            6
+          </button>
+          <button value="-" onClick={handleClick}>
+            -
+          </button>
+          <button value="1" onClick={handleClick}>
+            1
+          </button>
+          <button value="2" onClick={handleClick}>
+            2
+          </button>
+          <button value="3" onClick={handleClick}>
+            3
+          </button>
+          <button value="*" onClick={handleClick}>
+            *
+          </button>
+          <button value="C" onClick={handleClick}>
+            C
+          </button>
+          <button value="0" onClick={handleClick}>
+            0
+          </button>
+          <button value="=" onClick={handleClick}>
+            =
+          </button>
+          <button value="/" onClick={handleClick}>
+            /
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
